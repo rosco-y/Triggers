db52b1317d8397c67c8fb42ffc54d9f84cc5a37f
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-04-05 05:25:04 -0400

    Initial Commit

---
+++ Assets/scripts/doorOpener.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpener : MonoBehaviour
+{
+    public Transform door;
+    bool open = false;
+    public Vector3 openPosition;
+    public Vector3 closePosition;
+    public float doorSpeed;
+
+
+    private void Update()
+    {
+        if (open)
+        {
+            door.position = Vector3.Lerp(door.position, openPosition, Time.deltaTime * doorSpeed); 
+        }
+        else
+        {
+            door.position = Vector3.Lerp(door.position, closePosition, Time.deltaTime * doorSpeed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        open = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        open = false;
+    }
+}
---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    Rigidbody playerRB;
+    public float speed = 5;
+
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveH, 0, moveV);
+
+        playerRB.AddForce(move * Time.deltaTime * speed);
+
+    }
+}
